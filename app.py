c7b9f635622caf1d627d7533beb32573cf2d4c0f
JamesMungai254 <mungaijames6303@gmail.com>
2025-10-27 19:15:23 +0300

    model update

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 # 🧠 MODEL LOADING (Replace this part with your own)
 # ---------------------------
 def load_model():
-    model_path = "model/tb_model.pth"
+    model_path = "model/best_model.pth"
     model = torch.load(model_path, map_location=torch.device('cpu'))
     model.eval()
     return model
